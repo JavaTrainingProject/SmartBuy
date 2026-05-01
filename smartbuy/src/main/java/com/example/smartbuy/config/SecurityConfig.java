b577353c7e02d5484a3c7cd6d2bd1f84ef21ff92
SinguAkshaya <gayathri.b@clientservertech.com>
2026-05-01 18:48:44 +0530

    Added Frontend Configurationss

--- smartbuy/src/main/java/com/example/smartbuy/config/SecurityConfig.java
+++ smartbuy/src/main/java/com/example/smartbuy/config/SecurityConfig.java
@@ -19,7 +19,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity
 public class SecurityConfig {
 
-
     private final JWTFilter jwtFilter;
 
     public SecurityConfig(JWTFilter jwtFilter){
